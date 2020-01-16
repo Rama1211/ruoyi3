f31d5f3e7b0be49f11de411724e837c4df78515c
duguo <1293639342@qq.com>
2020-01-16 17:12:45 +0800

    学生信息，院系信息，专业信息查询service实现

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/MisStudentProfessionServiceImpl.java
@@ -0,0 +1,31 @@
+package com.ruoyi.system.service.impl;
+
+import com.ruoyi.system.domain.MisFaculty;
+import com.ruoyi.system.domain.MisProfession;
+import com.ruoyi.system.mapper.MisStudentFacultyMapper;
+import com.ruoyi.system.mapper.MisStudentMapper;
+import com.ruoyi.system.mapper.MisStudentProfessionMapper;
+import com.ruoyi.system.service.MisStudentFacultyService;
+import com.ruoyi.system.service.MisStudentProfessionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 专业信息 服务层处理
+ * 
+ * @author dg
+ */
+@Service
+public class MisStudentProfessionServiceImpl implements MisStudentProfessionService
+{
+    @Autowired
+    private MisStudentProfessionMapper professionMapper;
+
+
+    @Override
+    public List<MisProfession> selectProfessionList(String facultyId) {
+        return professionMapper.selectProfessionList(facultyId);
+    }
+}
